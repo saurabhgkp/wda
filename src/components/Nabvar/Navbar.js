8b7ba90731213d4dcea0903d4abd03246568e0a2
Saurabh Kumar Singh <saurabh.singh@team.cliffex.com>
2022-09-29 17:20:30 +0530

    fhuh

--- src/components/Nabvar/Navbar.js
+++ src/components/Nabvar/Navbar.js
@@ -1,130 +1,147 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
-import { gapi } from 'gapi-script';
-import { Link, useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { Helmet } from 'react-helmet-async'
+import React, { useEffect } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { gapi } from "gapi-script";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Helmet } from "react-helmet-async";
+import axios from "axios";
 const Navbar = () => {
-  const [token, setToken] = useState()
-  const navigate=useNavigate()
-  const clientId = '274492401016-j54651qg7maf329cdddq6aavlbb5a35l.apps.googleusercontent.com'
+  const navigate = useNavigate();
+  const clientId =
+    "274492401016-j54651qg7maf329cdddq6aavlbb5a35l.apps.googleusercontent.com";
 
-  // function login () {
-  //   var token = "gsgggsfgh";
-  //   localStorage.setItem("pwd",token)
-  //   setToken(token)
-  //   console.log(token);
-  // }
   function logout() {
-    console.log('logout')
-    localStorage.removeItem("pwd")
-    setToken()
-    navigate('/')
+    console.log("logout");
+    localStorage.removeItem("pwd");
+    navigate("/");
   }
-
 
   useEffect(() => {
     const initClient = () => {
       gapi.client.init({
         clientId: clientId,
-        scope: ''
+        scope: "",
       });
     };
-    gapi.load('client:auth2', initClient);
-  });
+    gapi.load("client:auth2", initClient);
+  }, []);
+
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "",
+      });
+    };
+    gapi.load("client:auth2", initClient);
+  }, []);
 
   const onFailure = (response) => {
     console.log(response);
-    if(response?.error!=='popup_closed_by_user')
-    toast.error("Might be an issue!! please try again later")
-  }
+    if (response?.error !== "popup_closed_by_user")
+      toast.error("Might be an issue!! please try again later");
+  };
 
-  const onSuccess = (res) => {
-    console.log(res?.accessToken);
-    localStorage.setItem('pwd', res?.accessToken)
-    setToken(res?.accessToken)
-  }
+  const onSuccess = async (res) => {
+    const { email, name } = res?.profileObj;
+    localStorage.setItem("pwd", email);
 
+    if (email) {
+      const data = await axios.post("http://localhost:4000/users/register", {
+        email,
+        name,
+      });
+    }
+  };
 
   return (
-    <>  
-<Helmet>
-
-
-
-  
-</Helmet>
-
-
-    <div
-      style={{
-        position: "sticky",
-        top: "0",
-        zIndex: "10",
-        //backgroundColor: "#051922",
-      }}
-    >
+    <>
+      <Helmet></Helmet>
       <div
-        className="top-header-area d-none d-md-block"
         style={{
-          backgroundColor: "#051922",
+          position: "sticky",
+          top: "0",
+          zIndex: "10",
+          //backgroundColor: "#051922",
         }}
       >
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12 col-sm-12 text-center">
-              <div className="main-menu-wrap">
-                <div className="site-logo">
-                  <Link to="/">
-                    <img src="assets/img/pcology1.png" alt="pcology" />
-                  </Link>
-                </div>
+        <div
+          className="top-header-area d-none d-md-block"
+          style={{
+            backgroundColor: "#051922",
+          }}
+        >
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12 col-sm-12 text-center">
+                <div className="main-menu-wrap">
+                  <div className="site-logo">
+                    <Link to="/">
+                      <img src="assets/img/pcology1.png" alt="pcology" />
+                    </Link>
+                  </div>
 
-                <nav className="main-menu">
-                  <ul>
-                    <li className="current-list-item">
-                      <Link to="/"><i class=" fa fa-home x-3"></i></Link>
-                    </li>
-                    <li>
-                      <Link to="/About">About</Link>
-                    </li>
-                    <li>
-                      <Link to="/Contact">Contact</Link>
-                    </li>
-                    <li>
-                      <Link to="/User">Shop</Link>
-                    </li>
+                  <nav className="main-menu">
+                    <ul>
+                      <li className="current-list-item">
+                        <Link to="/">
+                          <i class=" fa fa-home x-3"></i>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/About">About</Link>
+                      </li>
+                      <li>
+                        <Link to="/Contact">Contact</Link>
+                      </li>
+                      <li>
+                        <Link to="/User">Shop</Link>
+                      </li>
 
-                    <li> <li>
-                      <div class="header-icons p-3">
-
+                      <li>
+                        {" "}
                         <li>
-                          {/* {console.log(token)} */}
-                          {!localStorage.getItem('pwd') ?
-                            <GoogleLogin
-                              clientId={clientId}
-                              buttonText=""
-                              onSuccess={onSuccess}
-                              onFailure={onFailure}
-                              cookiePolicy={'single_host_origin'}
-                            ><h6>Login With Google</h6></GoogleLogin > :
-                            <div >  <a class="shopping-cart" href="/Cart"><i
-                              class=" fas fa-shopping-cart"></i></a>
-                              <GoogleLogout clientId={clientId} buttonText="" onLogoutSuccess={logout} ><h6>Logout</h6></GoogleLogout>
-                            </div>}
+                          <div class="header-icons p-3">
+                            <li>
+                              {/* {console.log(token)} */}
+                              {!localStorage.getItem("pwd") ? (
+                                <GoogleLogin
+                                  clientId={clientId}
+                                  buttonText=""
+                                  onSuccess={onSuccess}
+                                  onFailure={onFailure}
+                                  cookiePolicy={"single_host_origin"}
+                                >
+                                  <h6>Login With Google</h6>
+                                </GoogleLogin>
+                              ) : (
+                                <div>
+                                  {" "}
+                                  <a class="shopping-cart" href="/Cart">
+                                    <i class=" fas fa-shopping-cart"></i>
+                                  </a>
+                                  <GoogleLogout
+                                    clientId={clientId}
+                                    buttonText=""
+                                    onLogoutSuccess={logout}
+                                  >
+                                    <h6>Logout</h6>
+                                  </GoogleLogout>
+                                </div>
+                              )}
+                            </li>
+                          </div>
                         </li>
-
-                      </div>
-                    </li></li>
-                  </ul>
-                </nav>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-      </div>
-    </div> </>
+      </div>{" "}
+    </>
   );
 };
 
--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -3,28 +3,34 @@ import { Link } from "react-router-dom";
 import React from "react";
 const Product = () => {
   const [data, setData] = React.useState("");
+  const [item, setItem] = React.useState("");
   // const handelonUpdate = async (e) => {
   //   console.log(data);
   //   // e.preventDefault();
 
   //   const res = await axios.put('http://localhost:4000/users/addItem', data, {
-  //     params: {
-  //       email: email
+  //    {
+  //       email: email,
+  // itemid:item,
   //     },
   //   });
   // }
+
+  const additemfn = (e) => {
+    setItem(e);
+  };
+
+  console.log(item, "====================fsfd=============");
   React.useEffect(() => {
     fetch("http://localhost:4000/product/showAllProduct")
       .then((res) => res.json())
       .then((data) => {
-
         setData(data.data);
       });
   }, []);
   console.log("========", data && data[0]._id);
   return (
     <div>
-
       <div className="product-section mt-150 mb-150">
         <div className="container">
           <div className="row">
@@ -38,50 +44,57 @@ const Product = () => {
           </div>
 
           <div className="row">
-            {data && data.map((item, index) => {
-              return (
-                <>
-                  {item._id ? (
-                    <div className="col-lg-4 col-md-6  offset-md-3 offset-lg-0 text-center">
-                      <div className="single-product-item">
-                        <div className="product-image">
-                          <img src={item.image} alt="pc" />
-                        </div>
-                        <h3>{item.name}</h3>
-                        <p className="">{item.title} </p>
-                        <h4 className="product-price">
-                          ₹{item.price}{" "}
-                          <del className="product-old-price">
-                            ₹{item.old_price}
-                          </del>
-                        </h4>
-                        <Link to={`SingleProduct/${item._id}`}>
-                          <a className="cart-btn">
-                            <i className="fas fa-shopping-cart"></i> Shop Now
-                          </a>
-                        </Link>
-                        <div className=" row p-3"  >
-                          <div className="shopping-cart col ml" style={
-                            {
-                              color: "#F28123"
-                            }}>
-                            <a className="cart-btn"><i
-                              class=" fas fa-shopping-cart"></i> Add</a> </div>
-                          {/* <div className="shopping-cart col mr">
+            {data &&
+              data.map((item, index) => {
+                return (
+                  <>
+                    {item._id ? (
+                      <div className="col-lg-4 col-md-6  offset-md-3 offset-lg-0 text-center">
+                        <div className="single-product-item">
+                          <div className="product-image">
+                            <img src={item.image} alt="pc" />
+                          </div>
+                          <h3>{item.name}</h3>
+                          <p className="">{item.title} </p>
+                          <h4 className="product-price">
+                            ₹{item.price}{" "}
+                            <del className="product-old-price">
+                              ₹{item.old_price}
+                            </del>
+                          </h4>
+                          <Link to={`SingleProduct/${item._id}`}>
+                            <a className="cart-btn">
+                              <i className="fas fa-shopping-cart"></i> Shop Now
+                            </a>
+                          </Link>
+                          <div className=" row p-3">
+                            <div
+                              className="shopping-cart col ml"
+                              style={{
+                                color: "#F28123",
+                              }}
+                            >
+                              <div onClick={() => additemfn(item._id)}>
+                                <a className="cart-btn">
+                                  <i class=" fas fa-shopping-cart"></i> Add
+                                </a>{" "}
+                              </div>
+                            </div>
+                            {/* <div className="shopping-cart col mr">
                             <a className="cart-btn"
                             >
                               <i
                                 class=" fa fa-heart"></i>Favorite
                             </a></div> */}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  ) : (
-                    ""
-                  )}
-                </>
-              );
-            })}
+                    ) : (
+                      ""
+                    )}
+                  </>
+                );
+              })}
           </div>
         </div>
       </div>
